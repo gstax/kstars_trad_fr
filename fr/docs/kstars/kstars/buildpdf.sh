d9cf20b688f227fc70122abf63966337a56bfa82
stax <stax@ik.me>
2022-04-19 07:07:30 +0200

    Script pour générer le pdf

---
+++ fr/docs/kstars/kstars/buildpdf.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export SGML_CATALOG_FILES=/usr/share/sgml/docbook/sqml-dtd-4.5/catalog:/usr/share/kf5/kdoctools/customization/catalog.xml:/usr/share/sgml/docbook/xml-dtd-4.5/docbook
+# add -d to command below to keep the /tmp folder, so you can examine the generated tex.
+
+DBLATEX=$HOME/sources/docs-kde-org/dblatex-cvs-install/bin
+
+$DBLATEX/dblatex -d -b pdftex --style \
+	kdestyle\
+	-o $(pwd | awk -F/ '{ print $NF }').pdf \
+	-P latex.output.revhistory=0  -P newtbl.use=1 \
+	-P imagedata.default.scale=pagebound \
+	-P literal.width.ignore=1 \
+	-I $KDEDIR/share/doc/HTML/en/ \
+	-X \
+        $1
+
